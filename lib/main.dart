35e55b86dc952db6064d2655bea605ac19d6081b
willian-virgilio <willian.virgilio@gmail.com>
2021-12-21 14:44:59 -0300

    splashscreen import fixed

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Cat vs Dog',
+      home: MySplash(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
---
+++ lib/splashscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class Myslash extends StatefulWidget {
+  const Myslash({Key? key}) : super(key: key);
+
+  @override
+  _MyslashState createState() => _MyslashState();
+}
+
+class _MyslashState extends State<Myslash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

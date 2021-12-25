c036a32fa67f6e19ac43eeae3ad2d12511a2ec08
willian-virgilio <willian.virgilio@gmail.com>
2021-12-25 11:31:31 -0300

    final version

--- lib/home.dart
+++ lib/home.dart
@@ -98,7 +98,6 @@ class _HomeState extends State<Home> {
               SizedBox(
                 height: 50.0,
               ),
-              
               Container(
                   width: MediaQuery.of(context).size.width,
                   child: Column(
@@ -141,38 +140,40 @@ class _HomeState extends State<Home> {
                         ),
                       )
                     ],
-                  )
-                  ),
-              
-              
+                  )),
               Center(
-                child: _loading ? Container(
-                  width: 300,
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/cat.png'),
-                      SizedBox(
-                        height: 50.0,
+                child: _loading
+                    ? Container(
+                        width: 300,
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/cat.png'),
+                            SizedBox(
+                              height: 50.0,
+                            )
+                          ],
+                        ),
                       )
-                    ],
-                  ),
-                ):Container(child: Column(children: <Widget>[
-                  Container(
-                    height:250,
-                    child: Image.file(_image)
-                  ),
-                  SizedBox(
-                    height: 20,
-                    ),
-                    _output != null ? Text('${_output[0]['label']}',style: TextStyle(color: Colors.white,fontSize: 20)) :Container(),
-                ],)
-                ),
-                ),
-            ],
-              
+                    : Container(
+                        child: Column(
+                        children: <Widget>[
+                          Container(height: 250, child: Image.file(_image)),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          _output != null
+                              ? Container(
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  child: Text('${_output[0]['label']}',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20)),
+                                )
+                              : Container(),
+                        ],
+                      )),
+              ),
             ],
           ),
-        )
-        );
+        ));
   }
 }

670526f41820648aa6a593cf29e11bea2156e51d
willian-virgilio <willian.virgilio@gmail.com>
2021-12-28 19:19:06 -0300

    add the predction label output logical true or false on lines 157 to 162 of home.dart file

--- lib/home.dart
+++ lib/home.dart
@@ -148,6 +148,20 @@ class _HomeState extends State<Home> {
                                         borderRadius: BorderRadius.circular(10),
                                         child: Image.file(_image),
                                       ),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                    _output != null
+                                        ? Text(
+                                            'Prediction is: ${_output[0]['label']}',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 20),
+                                          )
+                                        : Container(),
+                                    SizedBox(
+                                      height: 30,
                                     )
                                   ],
                                 ),

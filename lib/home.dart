1fcba6ac1563a9ba4d51667b30816873e7ab6822
willian-virgilio <willian.virgilio@gmail.com>
2022-01-03 17:45:36 -0300

    change de model.tffile and labels
    increase number of results om clasifyImage function

--- lib/home.dart
+++ lib/home.dart
@@ -52,7 +52,7 @@ class _HomeState extends State<Home> {
   classifyImage(File image) async {
     var output = await Tflite.runModelOnImage(
       path: image.path,
-      numResults: 2,
+      numResults: 5,
       threshold: 0.5,
       imageMean: 127.5,
       imageStd: 127.5,
@@ -68,7 +68,7 @@ class _HomeState extends State<Home> {
 
   loadModel() async {
     await Tflite.loadModel(
-        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+        model: 'assets/model.tflite', labels: 'assets/labels.txt');
   }
 
   @override
